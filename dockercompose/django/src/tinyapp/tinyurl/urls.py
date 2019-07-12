52e0027f42baed9b88bb68c2ffc255fdce8928ba
Ram <dancerincloud@gmail.com>
2019-07-12 15:15:59 -0700

    regex for routes

--- dockercompose/django/src/tinyapp/tinyurl/urls.py
+++ dockercompose/django/src/tinyapp/tinyurl/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path
+from django.urls import re_path
+
 from . import views
 
 from .views import url_detail_view, make_tiny, get_original
@@ -7,7 +9,8 @@ from .views import url_detail_view, make_tiny, get_original
 urlpatterns = [
     path('', views.index, name='index'),
     path('url/', url_detail_view, name='urldetail' ),
-    path('maketiny/<url>', make_tiny, name='url_tiny' ),
+    #path('maketiny/<url>', make_tiny, name='url_tiny'),
+    re_path(r'^maketiny/(.+)', make_tiny, name='url_tiny'),
     path('<tinycode>/', get_original, name='go'),    
 ]
 
